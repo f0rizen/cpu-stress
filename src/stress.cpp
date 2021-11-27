0687567c74d8fbe9e64b27a66d25b2d2a189ecc0
f0rizen <root@f0rizen.tk>
2021-11-27 14:41:42 +0500

    Flags parsing

--- src/stress.cpp
+++ src/stress.cpp
@@ -1,25 +1,46 @@
+#include "argv.hpp"
 #include "int128.hpp"
 #include "primes.hpp"
 #include <algorithm>
 #include <chrono>
+#include <cstdlib>
 #include <omp.h>
+#include <string>
+
+bool isnumber(std::string str) {
+    for (int i = 0; i < (int)str.size(); ++i)
+        if (str[i] < '0' || str[i] > '9') return false;
+    return true;
+}
 
 int main(int argc, char *argv[]) {
-    omp_set_num_threads(8);
+    argvparse flags(argc, argv);
+    int th = 0;
+    if (!flags.flagExists("--threads")) {
+        th = omp_get_max_threads();
+        omp_set_num_threads(th);
+    }
+    else {
+        const std::string &threads = flags.getFlag("--threads");
+        if (threads.empty()) {
+            std::cout << "--threads <number of threads to use>\n";
+            exit(1);
+        }
+        if (!isnumber(threads)) {
+            std::cout << "--threads <NUMBER of threads to use>\n";
+            exit(1);
+        }
+        th = std::min(omp_get_max_threads(), atoi(threads.c_str()));
+        omp_set_num_threads(th);
+    }
+    std::cout << "Running on " << th << " threads\n";
+    std::cout << "Ctrl + C to stop\n";
     __int128 cur = 1000000;
-    std::chrono::steady_clock timer;
-    auto start = timer.now();
 #pragma omp parallel
     {
         while (true) {
-            auto end = timer.now();
-            auto time_span =
-                static_cast<std::chrono::duration<double>>(end - start).count();
-            if (time_span < double(std::stoi(argv[1]))) {
-                get_primes(cur);
-                ++cur;
-            } else
-                break;
+            get_primes(cur);
+            ++cur;
         }
     }
     return 0;
