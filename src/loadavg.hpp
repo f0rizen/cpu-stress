3b2f4484606f7a5ea18d7d064ffa8b9ab1a653ff
f0rizen <root@f0rizen.tk>
2021-11-27 18:53:51 +0500

    Load average

---
+++ src/loadavg.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+
+std::vector<double> getstats() {
+    double avg[3];
+    getloadavg(avg, 3);
+    std::vector<double> ans = {avg[0], avg[1], avg[2]};
+    return ans;
+}
--- src/stress.cpp
+++ src/stress.cpp
@@ -1,11 +1,14 @@
 #include "argv.hpp"
 #include "int128.hpp"
+#include "loadavg.hpp"
 #include "primes.hpp"
 #include <algorithm>
-#include <chrono>
-#include <cstdlib>
+#include <iomanip>
 #include <omp.h>
 #include <string>
+
+static const __uint128_t UINT128_MAX = __uint128_t(__int128_t(-1L));
+static const __int128_t INT128_MAX = UINT128_MAX >> 1;
 
 bool isnumber(std::string str) {
     for (int i = 0; i < (int)str.size(); ++i)
@@ -19,8 +22,7 @@ int main(int argc, char *argv[]) {
     if (!flags.flagExists("--threads")) {
         th = omp_get_max_threads();
         omp_set_num_threads(th);
-    }
-    else {
+    } else {
         const std::string &threads = flags.getFlag("--threads");
         if (threads.empty()) {
             std::cout << "--threads <number of threads to use>\n";
@@ -33,14 +35,17 @@ int main(int argc, char *argv[]) {
         th = std::min(omp_get_max_threads(), atoi(threads.c_str()));
         omp_set_num_threads(th);
     }
-    std::cout << "Running on " << th << " threads\n";
-    std::cout << "Ctrl + C to stop\n";
+    std::cout << "Running on " << th << " OpenMP thread(s)\n";
+    std::cout << "Ctrl + c to stop\n";
     __int128 cur = 1000000;
 #pragma omp parallel
     {
-        while (true) {
+        while (cur < INT128_MAX) {
             get_primes(cur);
             ++cur;
+            auto vec = getstats();
+            std::cout << std::flush << std::setw(3) << cur << " " << vec[0]
+                      << " " << vec[1] << " " << vec[2] << "\r";
         }
     }
     return 0;
